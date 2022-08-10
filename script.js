5ac35755e9198e410fd645ec2a139e743f0898cc
corina <pixmin@outlook.com>
2022-08-11 00:05:49 +0800

    Check for win function

--- script.js
+++ script.js
@@ -32,12 +32,31 @@ function changePlayer(){
 
 function checkForWin() {
     currentPlayerArray = []
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
     var fullArray = document.querySelectorAll(".field")
+    
     for (let i = 0; i < fullArray.length; i++) {
         if (fullArray[i].innerHTML == currentPlayer)
         currentPlayerArray.push(i)
     }
     console.log(currentPlayerArray)
+
+    for (let i = 0; i < winningCombinations.length; i++) {
+    result = winningCombinations[i].every(val => currentPlayerArray.includes(val))
+        if (result == true){
+            console.log("Player " + currentPlayer + " wins")
+        }
+    }
+
 }
 
 function restartGame() {
